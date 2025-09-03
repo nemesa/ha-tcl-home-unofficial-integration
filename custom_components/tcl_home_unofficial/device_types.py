dcac27d988fdf69808b4f6f163391ad2ac3208b0
iona-andras-nemes <andras.nemes@iongroup.com>
2025-09-03 11:35:58 +0200

    3.5.2 - handle split-ac with number

--- custom_components/tcl_home_unofficial/device_types.py
+++ custom_components/tcl_home_unofficial/device_types.py
@@ -9,15 +9,26 @@ class DeviceTypeEnum(StrEnum):
     DEHUMIDIFIER = "Dehumidifier"
 
 
+def is_split_ac_with_number(device_type:str)-> bool:
+    if device_type.lower().startswith("split ac-"):
+        suffix = device_type[9:]
+        if suffix.isdigit():
+            return True
+    return False
+
 def is_implemented_by_integration(device_type: str) -> bool:
     known_device_types = [
         "Split AC",
-        "Split AC-1",
+        "Split AC-#",
         "Split AC Fresh air",
         "Portable AC",
         "Window AC",
         "Dehumidifier DEM"
     ]
+    
+    if is_split_ac_with_number(device_type):
+        device_type = "Split AC-#"
+    
     if device_type.lower() in list(map(str.lower, known_device_types)):
         return True
     return False
@@ -32,6 +43,6 @@ def calculateDeviceType(device_type: str) -> DeviceTypeEnum | None:
         return DeviceTypeEnum.SPLIT_AC_FRESH_AIR
     elif device_type == "Window AC":
         return DeviceTypeEnum.WINDOW_AC
-    elif device_type == "Split AC" or device_type == "Split AC-1":
+    elif device_type == "Split AC" or is_split_ac_with_number(device_type):
         return DeviceTypeEnum.SPLIT_AC
     return None
